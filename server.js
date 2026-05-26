be950c8e003c85e25f8b717f87fc3fa1c6fcc691
motanafi <twaha2030@gmail.com>
2026-05-26 16:45:03 +0300

    connecting to db and setting up prisma

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 import express from "express";
+import pool from "./config/db.js";
 
 
 const app = express();
 
+
+
+// Test database connection
+pool.query('SELECT NOW()', (err, res) => {
+  if (err) {
+    console.error('Database connection error:', err);
+  } else {
+    console.log('Database connected! Server time:', res.rows[0].now);
+  }
+});
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
 const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
