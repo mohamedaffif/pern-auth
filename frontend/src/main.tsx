c1420b96c5dd98a0f66579d19d0bf41b6f9c465f
motanafi <twaha2030@gmail.com>
2026-05-28 23:28:35 +0300

    settign up react query

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,6 +7,13 @@ import {
   RouterProvider,
 } from '@tanstack/react-router'
 
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
+
 import { routeTree } from './routeTree.gen'
 
 const router = createRouter({
@@ -21,6 +28,9 @@ declare module '@tanstack/react-router' {
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
     <RouterProvider router={router} />
+    </QueryClientProvider>
+  
   </React.StrictMode>
 )
